4f72a6adf4ad71937a953004222a027127d3f5e0
paul <paulandrewbond@outlook.com>
2020-12-15 14:35:48 +0000

    Add ConfigureCors extension

--- FoodbankCollectiveAPI/Extensions/ServiceExtensions.cs
+++ FoodbankCollectiveAPI/Extensions/ServiceExtensions.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace FoodbankCollectiveAPI.Extensions
 {
-    public class ServiceExtensions
+    public static class ServiceExtensions
     {
-        public ServiceExtensions()
-        {
-        }
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
     }
 }
+
+//If we want to send requests from a different domain to our application, configuring CORS is mandatory. So, to start off, we’ll add a
+//    code that allows all requests from all origins to be sent to our API:
+//public static void ConfigureCors(this IServiceCollection services) =>
+//    services.AddCors(options =>
+//    {
+//        options.AddPolicy("CorsPolicy", builder =>
+//            builder.AllowAnyOrigin()
+//            .AllowAnyMethod()
+//            .AllowAnyHeader());
+//    });
