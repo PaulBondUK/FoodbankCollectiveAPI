354848b4a8db2c39066b0db88a5fbaa84f2ef0b4
paul <paulandrewbond@outlook.com>
2020-12-16 14:09:31 +0000

    Add LocationEntity class

---
+++ Entities/Models/LocationEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class LocationEntity
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "AddressLine1 is a required field.")]
+        [MaxLength(100, ErrorMessage = "Max length for AddressLine1 is 100 characters")]
+        public string AddressLine1 { get; set; }
+
+        [Required(ErrorMessage = "AddressTown is a required field")]
+        [MaxLength(30, ErrorMessage = "Max length for AddressTown is 30 characters")]
+        public string AddressTown { get; set; }
+
+        [Required(ErrorMessage = "AddressCounty is a required field")]
+        [MaxLength(30, ErrorMessage = "Max length for AddressCounty is 30 characters")]
+        public string AddressCounty { get; set; }
+
+        [Required(ErrorMessage = "AddressPostcode is a required field")]
+        [MaxLength(10, ErrorMessage = "Max length for AddressPostcode is 10 characters")]
+        public string AddressPostcode { get; set; }
+
+        [ForeignKey(nameof(FoodbankEntity))]
+        public Guid FoodbankId { get; set; }
+        public FoodbankEntity Foodbank { get; set; }
+
+    }
+}
