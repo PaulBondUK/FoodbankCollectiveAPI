9def8237b08f6df469e16b3c34fe86ad82b5b907
paul <paulandrewbond@outlook.com>
2020-12-17 14:40:57 +0000

    Add RepositoryContext to Entities project

---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Foodbank> Foodbanks { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+}
